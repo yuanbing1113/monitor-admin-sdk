2f1c1ee8d44eb5630dd82df42a928717163c545f
yuanbing1113 <yuanbing1113@gmail.com>
2021-04-15 15:46:52 +0800

    remove check external ip

--- pkg/ip.go
+++ pkg/ip.go
@@ -1,37 +1,35 @@
 package pkg
 
 import (
-	"encoding/json"
 	"fmt"
-	"io/ioutil"
 	"net"
-	"net/http"
 )
 
 // 获取外网ip地址
 func GetLocation(ip string) string {
-	if ip == "127.0.0.1" || ip == "localhost" {
-		return "内部IP"
-	}
-	resp, err := http.Get("https://restapi.amap.com/v3/ip?ip=" + ip + "&key=3fabc36c20379fbb9300c79b19d5d05e")
-	if err != nil {
-		panic(err)
+	// if ip == "127.0.0.1" || ip == "localhost" {
+	// 	return "内部IP"
+	// }
+	// resp, err := http.Get("https://restapi.amap.com/v3/ip?ip=" + ip + "&key=3fabc36c20379fbb9300c79b19d5d05e")
+	// if err != nil {
+	// 	panic(err)
 
-	}
-	defer resp.Body.Close()
-	s, err := ioutil.ReadAll(resp.Body)
-	fmt.Printf(string(s))
+	// }
+	// defer resp.Body.Close()
+	// s, err := ioutil.ReadAll(resp.Body)
+	// fmt.Printf(string(s))
 
-	m := make(map[string]string)
+	// m := make(map[string]string)
 
-	err = json.Unmarshal(s, &m)
-	if err != nil {
-		fmt.Println("Umarshal failed:", err)
-	}
-	if m["province"] == "" {
-		return "未知位置"
-	}
-	return m["province"] + "-" + m["city"]
+	// err = json.Unmarshal(s, &m)
+	// if err != nil {
+	// 	fmt.Println("Umarshal failed:", err)
+	// }
+	// if m["province"] == "" {
+	// 	return "未知位置"
+	// }
+	// return m["province"] + "-" + m["city"]
+	return "内部IP"
 }
 
 // 获取局域网ip地址
